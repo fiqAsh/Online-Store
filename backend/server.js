a3611ebab3714e503b07d4490d79277a776037f3
fiqAsh <ashfiq625@gmail.com>
2024-10-31 18:37:31 +0600

    initial commit with auth routes/controllers and database setup

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+
+//routes
+import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use("/api/auth", authRoutes);
+
+app.listen(5000, () => {
+	console.log("server on", PORT);
+
+	connectDB();
+});
