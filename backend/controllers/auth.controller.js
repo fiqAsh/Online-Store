ef37ba4e31f40d4de3d057c39df6800d008db1e3
fiqAsh <ashfiq625@gmail.com>
2024-10-31 21:10:21 +0600

    signup function

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,21 @@
+import User from "../models/user.model.js";
+
 export const signup = async (req, res) => {
-	res.send("sigup");
+	const { email, password, name } = req.body;
+
+	try {
+		const userExists = await User.findOne({ email });
+
+		if (userExists) {
+			return res.status(400).json({ message: "user already exists" });
+		}
+
+		const user = await User.create({ name, email, password });
+
+		res.status(201).json({ user, message: "user created successfully" });
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
 };
 export const login = async (req, res) => {
 	res.send("login");
