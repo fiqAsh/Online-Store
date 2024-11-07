ab5fa1b1c67e302188d0b4bb5088b8c11e10da60
fiqAsh <ashfiq625@gmail.com>
2024-11-07 22:44:49 +0600

    analytics route controller

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,8 @@ import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
 import couponRoutes from "./routes/coupon.route.js";
-
 import paymentRoutes from "./routes/payment.route.js";
+import analyticsRoutes from "./routes/analytics.route.js";
 //db
 import { connectDB } from "./lib/db.js";
 
@@ -25,6 +25,7 @@ app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/coupons", couponRoutes);
 app.use("/api/payments", paymentRoutes);
+app.use("/api/analytics", analyticsRoutes);
 
 app.listen(5000, () => {
 	console.log("server on", PORT);
