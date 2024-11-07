0f6dc75907ddde31355b05ec6f03403650275004
fiqAsh <ashfiq625@gmail.com>
2024-11-07 18:45:48 +0600

    initialize stripe for payment

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,9 @@ import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
 import couponRoutes from "./routes/coupon.route.js";
+
+import parmentRoutes from "./routes/payment.route.js";
+//db
 import { connectDB } from "./lib/db.js";
 
 dotenv.config();
@@ -21,6 +24,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/coupons", couponRoutes);
+app.use("/api/payments", paymentRoutes);
 
 app.listen(5000, () => {
 	console.log("server on", PORT);
